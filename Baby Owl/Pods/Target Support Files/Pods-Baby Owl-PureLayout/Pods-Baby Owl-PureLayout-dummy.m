c9c69802fc060ca30a835b86c1aaf6da5bf1d62e
Hao Lian <hi@haolian.org>
2015-11-22 16:11:30 -0500

    CocoaPods

---
+++ Baby Owl/Pods/Target Support Files/Pods-Baby Owl-PureLayout/Pods-Baby Owl-PureLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Baby_Owl_PureLayout : NSObject
+@end
+@implementation PodsDummy_Pods_Baby_Owl_PureLayout
+@end
---
+++ Baby Owl/Pods/Target Support Files/Pods-Baby Owl/Pods-Baby Owl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Baby_Owl : NSObject
+@end
+@implementation PodsDummy_Pods_Baby_Owl
+@end
